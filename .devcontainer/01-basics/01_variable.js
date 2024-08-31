60cbf9b4056b66476fea69ec18a3db85e963ec75
MohiBhaker <150266513+MohiBhaker@users.noreply.github.com>
2024-08-31 09:07:59 +0000

    "first basics"

---
+++ .devcontainer/01-basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let holderName = "Mohit"
+var accountPassword = "mohit123"
+accountCity = "Jaipur"
+
+// accountId = 2;
+
+
+holderName = "Mohit Kumar"
+accountPassword = "Jaipur@123"
+accountCity = "Manipur"
+
+
+console.log([accountId, holderName, accountPassword, accountCity]);
